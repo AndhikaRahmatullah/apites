87b37b126f09de6334a31115139e1363986750b5
AndhikaRahmatullah <dikarahmatullah909@gmail.com>
2022-07-10 23:52:22 +0700

    make navbar for sm screen

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 // methode date (tanggal)
 const tgl = document.querySelector(`#tgl`);
+const tgl2 = document.querySelector(`#tgl2`);
 let waktu = () => {
 	let months = ['Januari', 'Februari', 'Maret', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
 	let days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu']
@@ -16,9 +17,11 @@ let waktu = () => {
 	return `${h}, ${t} ${b} ${th}`
 }
 tgl.innerHTML = waktu();
+tgl2.innerHTML = waktu();
 
 // method date (jam)
 let jam = document.querySelector(`#jam`);
+let jam2 = document.querySelector(`#jam2`);
 function timeShow() {
 	let a_p = "";
 	let today = new Date();
@@ -41,6 +44,7 @@ function timeShow() {
 	curr_second = checkTime(curr_second);
 
 	jam.innerHTML = curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
+	jam2.innerHTML = curr_hour + ":" + curr_minute + ":" + curr_second + " " + a_p;
 }
 
 function checkTime(i) {
@@ -51,7 +55,17 @@ function checkTime(i) {
 }
 setInterval(timeShow, 500);
 
-// event change date to time
+// event show navbar-sm
+const hamburger = document.querySelector(`#hamburger`);
+hamburger.addEventListener('click', () => {
+	const nsm = document.querySelector(`#navbar-sm`);
+	const nsml = document.querySelector(`#navbar-sm-line`);
+	nsml.classList.toggle('hidden');
+	nsm.classList.toggle('hidden');
+	nsm.classList.toggle('grid');
+});
+
+// event change date to time (for md screen)
 const chg = document.querySelector(`#chg`);
 chg.addEventListener('mouseenter', () => {
 	const jam = document.querySelector(`#jam`);
@@ -65,6 +79,15 @@ chg.addEventListener('mouseleave', () => {
 	jam.classList.add('hidden');
 	tgl.classList.remove('hidden');
 })
+// event change date to time (for sm screen)
+const chg2 = document.querySelector(`#chg2`);
+chg2.addEventListener('click', () => {
+	const jam2 = document.querySelector(`#jam2`);
+	const tgl2 = document.querySelector(`#tgl2`);
+	jam2.classList.toggle('hidden');
+	tgl2.classList.toggle('hidden');
+})
+
 
 // event search
 const cari = document.querySelector(`#cari`);
